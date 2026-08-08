b31e9678654311208e2d7e951e5b8ba57e3fb166
Samir Perez <tu-correo-de-githubgmail.com>
2026-08-08 16:30:15 -0500

    feat: POST con titulo real - terminar mi portafolio

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-app = FastAPI(title='API Tareas - Samir Perez')
+app = FastAPI(title="API Tareas - Samir Perez")
+
+# Modelo de datos - así se hace en empresa real
+class Tarea(BaseModel):
+    titulo: str
+    hecha: bool = False
+
+tareas_db = [
+    {"id": 1, "titulo": "Aprender FastAPI", "hecha": False}
+]
 
 @app.get("/")
-def inicio(): 
-    return {"mensaje": "¡Bienvenido a mi API!"} 
+def inicio():
+    return {"mensaje": "¡Bienvenido a mi API!", "autor": "Samir - SENA"}
 
 @app.get("/tareas")
 def listar_tareas():
-    return [{"id":1, "titulo": 'Aprender FastAPI','hecha': False}]
+    return tareas_db
+
+@app.post("/tareas")
+def crear_tarea(tarea: Tarea):
+    nueva_tarea = {
+        "id": len(tareas_db) + 1,
+        "titulo": tarea.titulo,
+        "hecha": tarea.hecha
+    }
+    tareas_db.append(nueva_tarea)
+    return nueva_tarea
